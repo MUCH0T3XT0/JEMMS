1afca2e83920d5f9672b6defe937cd9708a74134
Mike <mike@MacBook-Pro-de-Mike.local>
2024-04-25 22:40:43 -0600

    Update: home

---
+++ controllers/home.controllers.js
@@ -0,0 +1,5 @@
+const model = require("../models/home.models.js");
+
+model.exports.render_cerrar_sesion = async(req, res)=>{
+    
+}
---
+++ routes/home.routes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("../controllers/home.controllers.js");
+
+router.get("/test_json", (req, res)=>{
+    req.status(200).json({code: 200, msg:"OK"});
+});
+
+router.get('/cerrar_sesion', controller.render_cerrar_sesion);
