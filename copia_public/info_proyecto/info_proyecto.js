fdaa5d29209839cd1b115723b14d990357d46fa9
Mike <mike@MacBook-Pro-de-Mike.local>
2024-05-15 13:08:36 -0600

    Se borraron archivos viejos

--- copia_public/info_proyecto/info_proyecto.js
+++
@@ -1,5 +0,0 @@
-const botonRegresar = document.getElementById("botonRegresar");
-
-botonRegresar.addEventListener("click", ()=>{
-    window.location.href = "/menu_proyecto";
-});
--- copia_public/reporte/reporte.js
+++
@@ -1,5 +0,0 @@
-const botonRegrear = document.getElementById("regresar");
-
-botonRegrear.addEventListener("click", ()=>{
-    window.location.href = "/menu_proyecto";
-});
