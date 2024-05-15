0010f99345930c776c05a4745b02d04af94bd572
Mike <mike@MacBook-Pro-de-Mike.local>
2024-05-15 15:19:29 -0600

    Se modifico: home

--- controllers/proyecto.controllers.js
+++ controllers/proyecto.controllers.js
@@ -5,12 +5,23 @@ module.exports.get_home = async(req,res) =>{
         console.log("Recuperando proyectos");
         const proyectos = await model.Proyecto.extraeProyectos();
 
+        if(proyectos.length < 1){
+            
+            res.render("home/home",{
+                error: true
+            });
+            return;
+        }
+
         res.render("home/home",{
-            proyecto: proyectos
+            proyecto: proyectos,
+            error: false
         });
     }catch(error){
         console.log(error);
-        res.render("home/home");
+        res.render("home/home",{
+            error: true
+        });
     }
     
 }
--- models/proyecto.models.js
+++ models/proyecto.models.js
@@ -2,23 +2,17 @@ const db = require('../utils/database.js');
 const bcrypt = require('bcryptjs');
 
 exports.Proyecto = class {
-    constructor(id, id_manager, descripcion, empresa, nombre_proyecto, presupuesto, f_creacion, f_fin, encargado, departamento){
+    constructor(id, descripcion, nombre_proyecto, estatus){
         this.id = id;
-        this.id_manager = id_manager;
         this.descripcion = descripcion;
-        this.empresa = empresa;
         this.nombre_proyecto = nombre_proyecto;
-        this.presupuesto = presupuesto;
-        this.f_creacion = f_creacion;
-        this.f_fin = f_fin;
-        this.encargado = encargado;
-        this.departamento = departamento;
+        this.estatus = estatus;
     }
 
     static async extraeProyectos(){
         try{
             const connexion = await db();
-            const resultado = await connexion.execute('Select * from PROYECTO');
+            const resultado = await connexion.execute('Select id_proyecto, descripcion, nombre_proyecto, estatus from PROYECTO');
             console.log(resultado);
 
             await connexion.release();
