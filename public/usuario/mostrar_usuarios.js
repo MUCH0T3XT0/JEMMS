9c1b4437085f9430176ab9bd0bc8852b346303df
Sofiosu24 <A01277695@tec.mx>
2024-05-24 12:56:17 -0600

    Modifcar las cabeceras de las tablas de mostrar usuarios

--- public/usuario/mostrar_usuarios.js
+++ public/usuario/mostrar_usuarios.js
@@ -5,25 +5,68 @@ window.addEventListener('load', function() {
     console.log("ABC");
 
         gridTable = new gridjs.Grid({
-        columns: [
-            "id_usuario", "nombres", "apellido_p", "nombre_proyecto"],
+            columns: [
+                {
+                    id: 'id_usuario',
+                    name: 'ID Usuario'
+                },
+                {
+                    id: 'nombres',
+                    name: 'Nombres'
+                },
+                {
+                    id: 'apellido_p',
+                    name: 'Apellido Paterno'
+                },
+                {
+                    id: 'nombre_proyecto',
+                    name: 'Nombre del Proyecto'
+                }
+            ],
         pagination: true,
         search: true,
         sort: true,
         server:{
             url: "/usuario/mostrar_usuarios_lideres",
-            then: data => data.usuario1
+            then: data => data.usuario1.map(usuario => [
+                usuario.id_usuario,
+                usuario.nombres,
+                usuario.apellido_p,
+                usuario.nombre_proyecto
+            ])
         }
     }).render(wrapper);
 
     gridTable1 = new gridjs.Grid({
-        columns: ["id_usuario", "nombres", "apellido_p", "correo"],
+        columns: [
+            {
+                id: 'id_usuario',
+                name: 'ID Usuario'
+            },
+            {
+                id: 'nombres',
+                name: 'Nombres'
+            },
+            {
+                id: 'apellido_p',
+                name: 'Apellido Paterno'
+            },
+            {
+                id: 'correo',
+                name: 'Correo Electrónico'
+            }
+        ],
         pagination: true,
         search: true,
         sort: true,
         server:{
             url: "/usuario/mostrar_usuarios_colaboradores",
-            then: data => data.usuario2
+            then: data => data.usuario2.map(usuario => [
+                usuario.id_usuario,
+                usuario.nombres,
+                usuario.apellido_p,
+                usuario.correo
+            ])
         }
     }).render(wrapper1);
 });
