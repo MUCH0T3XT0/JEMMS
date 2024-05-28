1d0c4bc15e75d4bb3e7dc83eeababf9b9c225d31
Emiliano Valdivia <a01276258@tec.mx>
2024-05-27 23:30:52 -0600

    Agregar comprobaciones adicionales a los datos

--- controllers/usuario.controllers.js
+++ controllers/usuario.controllers.js
@@ -93,16 +93,19 @@ module.exports.cerrar_sesion = async(req,res) => {
 module.exports.get_agregar_usuario = async(req,res) =>{
     const valido = req.query.valido === 'false' ? false : true;
     const correov = req.query.correov === 'false' ? false : true;
+    const nombre = req.query.nombre === 'false' ? false : true;
     res.status(200).render("agregar_usuario/agregar_usuario",{
         code:200,
         msg: "Ok",
         valido: valido,
-        correov:correov
+        correov:correov,
+        nombre:nombre
     });
 }
 
 module.exports.post_agregar_usuario = async(req, res) => {
     try {
+        const nombrevalido = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+$/;
         const nombre = req.body.nombre;
         const apellido_p = req.body.apellido_p;
         const apellido_m = req.body.apellido_m;
@@ -115,9 +118,19 @@ module.exports.post_agregar_usuario = async(req, res) => {
             return;
         }
 
-        const correovalido = /(\w|\d|\.|_)+@appix\.mx/;
+        const correovalido = /(\w|\.)+@appix\.mx/;
         if(!correovalido.test(correo)){
             res.status(403).redirect("/usuario/agregar_usuario?correov=false");
+            return;
+        }
+
+        if(!nombrevalido.test(nombre)||!nombrevalido.test(apellido_p)){
+            res.status(403).redirect("/usuario/agregar_usuario?nombre=false");
+            return;
+        }
+
+        if(apellido_m && !nombrevalido.test(apellido_m)){
+            res.status(403).redirect("/usuario/agregar_usuario?nombre=false");
             return;
         }
 
@@ -142,10 +155,7 @@ module.exports.post_agregar_usuario = async(req, res) => {
 
     } catch (error) {
         console.error(error);
-        res.status(500).redirect("/usuario/mostrar_usuarios",{
-            code: 500,
-            message: "Error registando usuario"
-        }); // Idealmente se crea una plantilla de errores genérica
+        res.status(500).redirect("/usuario/mostrar_usuarios?code:500&msg=Error+registando+usuario"); // Idealmente se crea una plantilla de errores genérica
     }
 }
 
@@ -154,6 +164,7 @@ module.exports.get_editar_usuario = async(req,res) =>{
         //Busca el usuario en la BD
         const usuario = await model.Usuario.buscaUsuarioPorId(req.params.id);
         const valido = req.query.valido === 'false' ? false : true;
+        const nombre = req.query.nombre === 'false' ? false : true;
 
         //Verifica la que exista el Usuario de la BD, si es menor a 1 significa que es un array vacio
         if(usuario.length < 1){
@@ -166,7 +177,8 @@ module.exports.get_editar_usuario = async(req,res) =>{
             code: 200,
             msg: "Ok",
             usuario:usuario,
-            valido:valido
+            valido:valido,
+            nombre:nombre
         });
     }catch (error){
         res.redirect("/agregar_usuario/agregar_usuario");
@@ -176,6 +188,7 @@ module.exports.get_editar_usuario = async(req,res) =>{
 module.exports.post_editar_usuario = async(req, res) => {
     try {
         //Se guarda la info del body en constantes
+        const nombrevalido = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+$/;
         const id = req.params.id;
         const nombre = req.body.nombre;
         const apellido_p = req.body.apellido_p;
@@ -186,6 +199,16 @@ module.exports.post_editar_usuario = async(req, res) => {
 
         if(nombre == ''||apellido_p == ''||correo == ''|| contrasena == ''){
             res.status(400).redirect(`/usuario/${id}/editar_usuario?valido=false`);
+            return;
+        }
+
+        if(!nombrevalido.test(nombre)||!nombrevalido.test(apellido_p)){
+            res.status(403).redirect(`/usuario/${id}/editar_usuario?nombre=false`);
+            return;
+        }
+
+        if(apellido_m && !nombrevalido.test(apellido_m)){
+            res.status(403).redirect(`/usuario/${id}/editar_usuario?nombre=false`);
             return;
         }
         
